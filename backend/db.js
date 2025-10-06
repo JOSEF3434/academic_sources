150c7d305d12a06ac3bdacccae4a670254359acd
YOSEF <152890781+JOSEF3434@users.noreply.github.com>
2025-10-06 14:02:45 +0300

    Initial commit: project setup and backend files for academic_sources project

---
+++ backend/db.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const uri = "mongodb://localhost:27017/fethAI"
+//const uri = "mongodb+srv://jociemane:cLUk08bsFSokr023@cluster0.8h6jv.mongodb.net/fethAIy?retryWrites=true&w=majority&appName=Cluster0";
+
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => {
+  console.log("MongoDB connected successfully");
+  console.log("Connected to database:", mongoose.connection.name);
+})
+.catch(err => {
+  console.error("MongoDB connection error:", err);
+  console.error("Error details:", {
+    name: err.name,
+    message: err.message,
+    code: err.code
+  });
+});
